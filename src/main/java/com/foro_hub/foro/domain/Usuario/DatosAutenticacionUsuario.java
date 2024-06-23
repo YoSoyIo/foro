142c54cf4b2ff74d81082a00ca99130f61e609fc
YoSoyIo <yosoyio057@gmail.com>
2024-06-23 11:34:53 -0600

    Authenticacion JWT

---
+++ src/main/java/com/foro_hub/foro/domain/Usuario/DatosAutenticacionUsuario.java
@@ -0,0 +1,7 @@
+package com.foro_hub.foro.domain.Usuario;
+
+public record DatosAutenticacionUsuario(
+        String login,
+        String clave
+) {
+}
--- src/main/java/com/foro_hub/foro/infra/security/SecurityConfigurations.java
+++ src/main/java/com/foro_hub/foro/infra/security/SecurityConfigurations.java
@@ -2,9 +2,13 @@ package com.foro_hub.foro.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +16,20 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigurations {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf().disable().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
